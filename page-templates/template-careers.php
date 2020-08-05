0aaf5588e3bf829c833f62c246b35c99827db7b7
farhadfarhadii <29977968+farhadfarhadii@users.noreply.github.com>
2020-08-05 18:36:14 +0100

    jobs pulled from greenhouse.

--- page-templates/template-careers.php
+++ page-templates/template-careers.php
@@ -8,86 +8,71 @@
 
 get_header(); ?>
 
+<?php
+	// Get a list of job vaccanies from greenhouse.
+    $url = 'https://boards-api.greenhouse.io/v1/boards/seldon/jobs';
+
+    try {
+		$contents = json_decode(file_get_contents($url));
+    } catch(Exception $err) {
+		// echo the error server side for debugging.
+        echo $err->getMessage();
+    }
+?>
+
 <main>
 	<div class="container-fluid">
 
-		<?php while ( have_posts() ) : the_post(); ?>
+		<?php get_template_part( 'sections/page-hero' ); ?>
 
-			<?php get_template_part( 'sections/page-hero' ); ?>
+		<?php get_template_part('sections/intro-block'); ?>		
 
-			<?php get_template_part('sections/intro-block'); ?>		
-
-			<?php get_template_part('sections/content-blocks'); ?>
-
-
-<?php if (have_rows('careers') ) : ?>
-	<div id="CareersListing" class="row mt--30 bg_lightgrey pb-80">
-		<div class="col-sm-12 maxout">
-
+		<?php get_template_part('sections/content-blocks'); ?>
+	
+		<div id="CareersListing" class="row mt--30 bg_lightgrey pb-80">
+			<div class="col-sm-12 maxout">
 			<div class="row">
 				<div class="col-sm-11 col-sm-offset-1">
-					
-					<h2 class="mb-50 section-headline">Current vacancies</h2>
-					<?php while (have_rows('careers')) : the_row();
-						$position = get_sub_field( 'position' );
-						$link = get_sub_field( 'link' );
-						$description = get_sub_field( 'description' );
-						?>
-
-						<div class="row career-block bg_yellow mv-20">
-
-							<div class="job-title col-sm-5 col-md-4 pos-relative match-height-career-block">
-								<h3>							
-								<a href="<?php echo $link; ?>" target="_blank"><?php echo $position; ?></a>
-								</h3>
-
+					<?php if($contents && $contents->meta->total !== 0 ):?>
+						<h2 class="mb-50 section-headline">Current vacancies</h2>
+					<?php endif; ?>
+					<div class="row career-block bg_yellow mv-20"
+						<?php if ($contents && $contents->meta->total !== 0 ):?>
+							<?php foreach ($contents->jobs as $job):?>
+								<div class="job-title col-sm-5 col-md-4 pos-relative match-height-career-block">
+									<h3>							
+									<a href="<?php echo $job->absolute_url ?>" target="_blank"><?php echo $job->title ?></a>
+									</h3>
+								</div>
+								<div class="job-description col-sm-7 col-md-8 col-sm-pad-left-1 col-sm-pad-right-1 bg_black pv-60 match-height-career-block">
+								<?php echo html_entity_decode($job->content, ENT_QUOTES, 'utf8'); ?>
+								<div>
+									<a href="<?php echo $job->absolute_url ?>" target="_blank" class="btn margin-top-20">Read More</a>
+								</div>
 							</div>
-
-							<div class="job-description col-sm-7 col-md-8 col-sm-pad-left-1 col-sm-pad-right-1 bg_black pv-60 match-height-career-block">
-								<?php echo $description; ?>
-
-								<a href="<?php echo $link; ?>" target="_blank" class="btn margin-top-20">Read More</a>
-
+						<?php endforeach; endif; ?>
+					</div>						
+				</div>
+			</div>
+		</div>
+		<?php 
+			$video_url = get_field( 'video_url' );
+			if ($video_url):
+		?>
+			<div id="CareersVideo" class="row mt--15">
+				<div class="col-xs-12 pt-100 pb-100 mt--30 bleed_full bg_black">
+					<div class="row maxout">
+						<div class="col-sm-11 col-sm-offset-1">
+							<div class="embed-responsive embed-responsive-16by9">
+								<iframe class="embed-responsive-item" src="//www.youtube.com/embed/d9qAjkAHtWI"></iframe>
 							</div>
 
 						</div>
-
-					<?php endwhile; ?>
-
-				</div>			
-			</div>
-		</div>
-	</div>
-<?php endif; ?>
-
-<?php 
-	$video_url = get_field( 'video_url' );
-	if ($video_url) { ?>
-<div id="CareersVideo" class="row mt--15">
-	<div class="col-xs-12 pt-100 pb-100 mt--30 bleed_full bg_black">
-		<div class="row maxout">
-			<div class="col-sm-11 col-sm-offset-1">
-
-				<div class="embed-responsive embed-responsive-16by9">
-				    <iframe class="embed-responsive-item" src="//www.youtube.com/embed/d9qAjkAHtWI"></iframe>
+					</div>
 				</div>
-
 			</div>
-		</div>
+		<?php endif; ?>
 	</div>
-	
-</div>
-
-
-
-
-<?php } ?>
-
-
-
-		<?php endwhile; ?>
-	</div>
-
 </main>
 
 <?php get_footer();
