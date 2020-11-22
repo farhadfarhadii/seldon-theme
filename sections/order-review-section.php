cc11613f7665b94ac2961cd59b505892b724eea6
farhadfarhadii <29977968+farhadfarhadii@users.noreply.github.com>
2020-11-22 19:58:01 +0000

    removed newsletter sign up

--- sections/order-review-section.php
+++ sections/order-review-section.php
@@ -70,9 +70,6 @@
                             <div class="order--checkbox">
                                 <input required type="checkbox" /><span>I have read the <a href="">terms and conditions.</a></span>
                             </div>
-                            <div class="order--checkbox">
-                                <input checked type="checkbox" /><span>I would like to sign up for the newsletter.</span>
-                            </div>
                             <div class="order--disclaimer">
                                 <p>By clicking on the confirm order button, you hereby accept that you are authorized to perform this transaction with the selected payment method.</p>
                             </div>
